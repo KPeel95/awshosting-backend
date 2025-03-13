c4bd38a6ec3c7d230adbf938d8aa70444c3a141b
KPeel95 <95351926+KPeel95@users.noreply.github.com>
2025-03-13 15:04:15 +0000

    Update index.js
    port changed to 8080

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const todoRoutes = require('./routes/todo');
 
 //APP SETTINGS
 app.use(express.json());
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 8080;
 app.use(cors());
 
 
