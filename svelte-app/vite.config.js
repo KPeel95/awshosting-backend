75a7e774acf205b5ca41f1793f32b3ffcb35d105
Kieran Peel <Kieran.Peel@standardclub.com>
2025-03-17 14:39:48 +0000

    Removed rollup files

--- svelte-app/vite.config.js
+++ svelte-app/vite.config.js
@@ -3,5 +3,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     outDir: '../public',  // Specify the output directory
+    rollupOptions: {
+      input: './index.html',  // Ensure Vite knows where to find the entry point
+    },
   }
 });
